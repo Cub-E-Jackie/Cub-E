c23aa6bf454c18e4632cda840ec6bdc672bd9628
Cube-E-Conny <40527283+Cube-E-Conny@users.noreply.github.com>
2018-07-10 13:22:43 +0200

    Übungen von N. Pattmann
    Collider, Trigger und zufällige Positionierung von Hindernissen

---
+++ PlacementScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementScript : MonoBehaviour {
+
+    // globale Variablen
+    public GameObject hindernis;
+
+    private float timeCounter = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeCounter += Time.deltaTime;
+        if (timeCounter > 1f)
+        {
+            timeCounter = 0f;
+            ErzeugeHindernis();
+        }
+	}
+
+
+    private void ErzeugeHindernis()
+    {
+        Vector3 startPoint = new Vector3(Random.Range(-5f, 5), 10f, Random.Range(-5f, 5f));
+        Instantiate(hindernis, startPoint, Quaternion.identity);
+    } 
+}
---
+++ TriggeredEvent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggeredEvent : MonoBehaviour {
+
+    public float lifetime = 10f;
+    private float timer = 0;
+	
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > lifetime)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        GetComponent<MeshRenderer>().material.color = Color.green;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Ground")
+        {
+            GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+
+
+}
