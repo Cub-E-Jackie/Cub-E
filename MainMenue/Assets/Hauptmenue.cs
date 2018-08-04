6c424403169183a73d8fc81808431a6b271d2b73
Cube-E-Anne <40500532+Cube-E-Anne@users.noreply.github.com>
2018-08-04 15:09:22 +0200

    Add files via upload
    Das ist das  Startmenü. Es müssen aber noch der Button QUIT und der Button PLAY verbunden werden. Kann ich machen, wenn wir das Spiel haben

---
+++ MainMenue/Assets/Hauptmenue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //braucht man um die verschiedenen Level zu koordinieren
+
+public class Startmenue : MonoBehaviour {
+
+	// Start Game:
+	public void PlayGame() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // immer ein Level addieren, erst Level eins, dann zwei... bzw. man kommt zum Spiel 
+		//ACHTUNG!!!: Spiel und Menü müssen miteinander verbunden werden! wie? Tutorial: "START MENU in Unity" auf YouTube
+		
+	}
+	
+	// Quit Game:
+	public void  QuitGame() {
+		Debug.Log("QUIT"); //Info wird geschrieben
+		Application.Quit(); //Beendet Spiel
+	}
+}
